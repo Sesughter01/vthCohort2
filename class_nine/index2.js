05c42d66bd9e1a2eeff5c5f2d9e38ab981809720
Bigchevrrr <sheriffayobami4321@gmail.com>
2023-08-21 17:36:17 +0100

    index2

---
+++ class_nine/index2.js
@@ -0,0 +1,22 @@
+const Bankaccount = {
+    firstName : 'Sheriff',
+    lastName : 'Nasr',
+    Gender : 'Male',
+    Age : '22',
+    Address: 'Pipe-line road, Magboro',
+    Bvn:'22223334458',
+    occupation: 'jamonjamon',
+    Accounttype:'individuals',
+
+    display(){
+        return "Name: "+this.firstName +"<br>" + "lastName: " + this.lastName + "<br>" + "Gender: "+ this.Gender + " <br>" +"Age:" +this.Age + "<br>" + "Address:" +this.Address + " <br>" + "occupation:" +this.occupation + "<br> " + "Accounttype:" +this.Accounttype + "<br>"
+    }
+
+
+}
+document.getElementById("btn").addEventListener('click', ()=>{
+     var d = document.getElementById("display").innerHTML = Bankaccount.display();
+     d = " "
+
+
+ })

bac3eedfec7c6452ea97b37ff58746bbb4191a84
Feranmi <feranmiphillp024@gmail.com>
2023-08-21 17:07:55 +0100

    class nine

---
+++ class_nine/index2.js
@@ -0,0 +1,13 @@
+const bankaccount = {
+    firstName: "Oluwaferanmi",
+    lastName: "Adesida",
+    acctNo: "8063868707",
+    bankname: "Opay",
+
+
+display(){
+    return "AccountName:" + this. firstName +  "<br>Second name:" + this.lastName + "<br>Accountnumber:" + this. acctNo + "<br>Bankname:" + this.bankname;
+
+}
+ 
+};

807788ebd7354fd9915e6d24b8927749fe422161
Bigchevrrr <sheriffayobami4321@gmail.com>
2023-08-28 13:28:30 +0100

    message

--- class_nine/index2.js
+++ class_nine/index2.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const Bankaccount = {
     firstName : 'Sheriff',
     lastName : 'Nasr',
@@ -20,3 +21,19 @@ document.getElementById("btn").addEventListener('click', ()=>{
 
 
  })
+=======
+const bankAccount = {
+    account: 'Sterling Bank',
+    acctNo: '0123456789',
+    amount: '22000',
+    openedOn: '31st July 2004',
+    expiresOn: '1st may 2069',
+    demo2(){
+        return "Account Name: " + this.account + "<br>Account Number: " + this.acctNo + "<br>Total Balance: " + this.amount 
+        + "<br>Opened on: " + this.openedOn + "<br>Will Expire In: " + this.expiresOn;
+    
+    }
+  
+  
+    };
+>>>>>>> victorherald

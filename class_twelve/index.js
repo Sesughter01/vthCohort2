372d798feee3c577ee237580bab6c3334deb0b08
Sesughter01 <charles.avul@gmail.com>
2023-08-30 12:51:26 +0100

    updated class_twelve again

--- class_twelve/index.js
+++ class_twelve/index.js
@@ -5,13 +5,43 @@ function displayDate(){
 
 const btn = document.querySelector("#color_btn");
 
-function random(num, ) {
-  return Math.floor(Math.random() * (num + 1));
+// function random(number) {
+//   return Math.floor(Math.random() * (number + 1));
+// }
+const random = (number)=>{
+  return Math.floor(Math.random() * (number + 1));
 }
 
-// Callback function is ?
 btn.addEventListener("click", () => {
   const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
   document.body.style.backgroundColor = rndCol;
 });
 
+
+const span = document.querySelector('.select');
+const classes = span.classList;
+
+span.addEventListener('click',()=>{
+
+   const result = classes.toggle('c');
+   span.textContent = `'c' ${
+
+     result ? "added" : "removed"
+   }; classlist is now "${classes}".`
+})
+
+/**
+ * 
+ * 
+ *   function nameOfFunctn(){
+ *     do somethng
+ * }
+ * 
+ * Arrow function
+ * 
+ *   ()=>{
+ *    do somethng 
+ * }
+
+
+ */

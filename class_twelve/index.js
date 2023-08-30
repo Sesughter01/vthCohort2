29f9d44881277d99b78a8c35430ce4a8ee08b5e4
Jacky <Pulpyshegz@gmail.com>
2023-08-30 12:57:39 +0100

    button rework

--- class_twelve/index.js
+++ class_twelve/index.js
@@ -3,7 +3,7 @@ function displayDate(){
     document.getElementById('text').innerHTML=Date();
    }
 
-const btn = document.querySelector("button");
+const btn = document.querySelector("#colorBtn");
 
 function random(number) {
   return Math.floor(Math.random() * (number + 1));
@@ -13,3 +13,15 @@ btn.addEventListener("click", () => {
   const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
   document.body.style.backgroundColor = rndCol;
 });
+
+
+// let x = 1
+// if (x) {
+//   alert("true")
+//   // return "true"
+
+// }
+// else{
+//   alert("false")
+// }
+

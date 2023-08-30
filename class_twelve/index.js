2e9ceff06a00c4ae6e64ab491704800b15c98fcc
Bigchevrrr <sheriffayobami4321@gmail.com>
2023-08-30 13:08:25 +0100

    ghsgsjud

--- class_twelve/index.js
+++ class_twelve/index.js
@@ -13,3 +13,14 @@ btn.addEventListener("click", () => {
   const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
   document.body.style.backgroundColor = rndCol;
 });
+
+
+const commt = null
+
+if (commit){
+  console.log("true")
+}
+
+ else{
+  console.log("false")
+}

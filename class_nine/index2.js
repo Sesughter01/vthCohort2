0ad2938995ef785b81beb0afcdc2779fb97ae464
Bigchevrrr <sheriffayobami4321@gmail.com>
2023-09-04 12:25:25 +0100

    mess

--- class_nine/index2.js
+++ class_nine/index2.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Bankaccount = {
     firstName : 'Sheriff',
     lastName : 'Nasr',
@@ -15,25 +14,39 @@ const Bankaccount = {
 
 
 }
-document.getElementById("btn").addEventListener('click', ()=>{
-     var d = document.getElementById("display").innerHTML = Bankaccount.display();
+// document.getElementById("btn").addEventListener('click', ()=>{
+//      var d = document.getElementById("display").innerHTML = Bankaccount.display();
      
 
+//  })
 
- })
-=======
-const bankAccount = {
-    account: 'Sterling Bank',
-    acctNo: '0123456789',
-    amount: '22000',
-    openedOn: '31st July 2004',
-    expiresOn: '1st may 2069',
-    demo2(){
-        return "Account Name: " + this.account + "<br>Account Number: " + this.acctNo + "<br>Total Balance: " + this.amount 
-        + "<br>Opened on: " + this.openedOn + "<br>Will Expire In: " + this.expiresOn;
+ const btn=document.querySelector('#botin')
+
+ const container=document.getElementById('display')
+ const btnClasses=btn.classList
+
+
+
+
+ btn.addEventListener('click',()=>{
+    const checker=btnClasses.toggle('c')
+    if (checker){
+       container.innerHTML = Bankaccount.display();
+         
+        display()
+
+     }
+     else{
+        container.innerHTML = ""
+
     
     }
-  
-  
-    };
->>>>>>> victorherald
+     
+     }
+
+ 
+
+
+
+ 
+ )

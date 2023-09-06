74af9a0d7651aac86f0ff07e4e897c009d8f1149
Bigchevrrr <sheriffayobami4321@gmail.com>
2023-09-06 14:02:34 +0100

    message

--- class_14/index.js
+++ class_14/index.js
@@ -1,1 +1,41 @@
 const models = ['user','student','teachers','subject']
+const models2 = []
+let grab = document.querySelector('#second')
+let bag = document.querySelector('.second')
+
+let ogbonno = document.querySelector('.jink')
+let gbah = document.querySelector('.sheffs')
+let text = ''
+
+models.forEach(addContent);
+
+function addContent(value, index, array){
+   gbah.innerHTML+=`${value} <br>`
+
+    ogbonno.appendChild(gbah)
+}
+
+models[0] = 'Rice'
+models2.push(models[0])
+models[1] = 'Beans'
+models2.push(models[1])
+models[3] = 'Jollof'
+models2.push(models[2])
+models[4] = 'dodo'
+models2.push(models[3])
+
+
+
+
+
+models2.forEach(add2);
+
+function add2(value, index, array){
+    bag.innerHTML+=`${value} <br>`
+}
+
+
+
+//appendChild(text)
+
+//models[0, 1, 2, 3] = 'Login', 'Password', 'gear', 'brake';
--- class_twelve/index.js
+++ class_twelve/index.js
@@ -17,15 +17,7 @@ btn.addEventListener("click", () => {
   document.body.style.backgroundColor = rndCol;
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
  element.style.transform = "translateX(0%)";
->>>>>>> ef2805fd7ffaf273252add4e7c7d206b584cb132
-=======
- element.style.transform = "translateX(0%)";
->>>>>>> 47d0aa869fc65736d2ee7f833681d26da9157c2c
 const span = document.querySelector('.select');
 const classes = span.classList;
 

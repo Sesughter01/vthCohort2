025555177f6fbdc075ad9fecfcd6fc22fc5b1d79
Sesughter01 <charles.avul@gmail.com>
2023-10-04 16:37:02 +0100

    added class 21

---
+++ class_21/returnStr.js
@@ -0,0 +1,14 @@
+/** 
+*  This package takes a value of any datatype 
+   turns it into a string and returns it 
+*/ 
+
+export const  returnStr = (strs)=>{
+    return strs;
+}
+export const  returnStr2 = (strs)=>{
+    return strs;
+}
+
+// export default  returnStr
+// export default  returnStr2

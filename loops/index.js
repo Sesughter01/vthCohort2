c65c2968a8d5cc09a44cfd63992e6806f3c6a23a
happiess77 <danielchinyere77@gmail.com>
2023-11-29 07:54:31 -0800

    updated changes

--- loops/index.js
+++ loops/index.js
@@ -1,1 +1,15 @@
-// Do class work here
+// create a loop that runs from 1 to 15
+
+var text = "";
+    
+    for (let i = 0; i < 16; i++) {
+      text += " " + i + "<br>";
+    }
+    document.getElementById("demo").innerHTML = text;
+    // create an array with 3 element
+      const cars = ["BMW", "Volvo", "Saab"];
+      let i, len, text;
+      for (i = 0, len = cars.length, text = ""; i < len; i++) {
+      text += cars[i] + "<br>";
+}
+document.getElementById("demo2").innerHTML = text;

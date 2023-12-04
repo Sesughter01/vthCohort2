011dcac803b2d69914ebea5b77b54aa11e5b6642
bisolaharuna <abisola.haruna@gmail.com>
2023-12-04 11:20:35 +0100

    loops class

---
+++ loops/index.js
@@ -0,0 +1,9 @@
+// Do class work here
+// create a loop that displays numbers from 1 to 15 in the browser
+// var p=""
+ for (let i = 1, p=""; i < 16; i++) {
+    
+    p+=i + "<br>"
+    document.getElementById("Hello").innerHTML=p;
+}
+

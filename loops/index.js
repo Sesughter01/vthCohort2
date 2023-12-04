3d326f51570d5764e3092f7d50b586f5356ae0c8
olomosolaemmanuel <olomosolaemmanueltosin@gmail.com>
2023-12-04 11:25:22 +0100

    --

--- loops/index.js
+++ loops/index.js
@@ -1,1 +1,11 @@
 // Do class work here
+// let text = "";
+//         for(let i= 1; i< 16; i++) {
+//             text += "the number is" + i + "<br>";
+//         }
+//         document.getElementById("demo").innerHTML = text;
+        
+const fruits = ["Apple", "Banana", "Orange"];
+for (x fruits) {
+  
+}

5539090aa28839c1b06470b743b4c2b0eb2d4ef0
AishaBakare <bakareaisha23@gmail.com>
2023-12-04 11:25:05 +0100

    changes in index.js

--- loops/index.js
+++ loops/index.js
@@ -1,1 +1,33 @@
 // Do class work here
+// let text = "";
+
+// for (let i = 1; i < 16; i++) {
+//   text += "The number is " + i + "<br>";
+// }
+// document.getElementById("demo").innerHTML = text;
+
+// const fruits = ["Apple","Banana","Orange"]
+// for ( x in fruits) {
+
+// }
+
+//     document.getElementById("demo").innerHTML = fruits;
+
+// let x = myfunction(4,3)
+// document.getElementById("demo").innerHTML = x
+
+function myfunction(a,b){
+    return a * b;
+
+}
+myfunction(4,3)
+/*Arrow function*/
+const myfunction = b => b * c
+
+const myfunction = (b,c) => {
+
+}
+
+const myfunction = function(b,d){
+console.log(b)
+}

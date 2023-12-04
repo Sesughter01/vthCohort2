ce6455b661e626437994841180c895d014e4762c
happiess77 <danielchinyere77@gmail.com>
2023-12-04 00:21:48 -0800

    updated

--- loops/index.js
+++ loops/index.js
@@ -1,15 +1,15 @@
 // create a loop that runs from 1 to 15
 
-var text = "";
+// var text = "";
     
-    for (let i = 0; i < 16; i++) {
-      text += " " + i + "<br>";
-    }
-    document.getElementById("demo").innerHTML = text;
+    // for (let i = 1; i < 16; i++) {
+      // text += " " + i + "<br>";
+    // }
+    // document.getElementById("demo").innerHTML = text;
     // create an array with 3 element
       const cars = ["BMW", "Volvo", "Saab"];
       let i, len, text;
       for (i = 0, len = cars.length, text = ""; i < len; i++) {
       text += cars[i] + "<br>";
 }
-document.getElementById("demo2").innerHTML = text;
+document.getElementById("demo").innerHTML = text;

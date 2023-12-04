4e53cf4ff39670b6091eb5a5c23e63c519160dc3
happiess77 <danielchinyere77@gmail.com>
2023-12-04 07:11:58 -0800

    updated

--- modules/grids/grid.js
+++ modules/grids/grid.js
@@ -1,1 +1,41 @@
 // create javascript code
+// Create Object from JSON String
+let text = '{"employees":[' +
+'{"firstName":"John","lastName":"Doe" },' +
+'{"firstName":"Anna","lastName":"Smith" },' +
+'{"firstName":"Peter","lastName":"Jones" }]}';
+
+const obj = JSON.parse(text);
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+// JavaScript Debugger
+let x = 15 * 5;
+debugger;
+document.getElementById("demo2").innerHTML = x;
+// JavaScript Functions
+function myFunction(a, b) {
+    return a * b;
+  }
+  document.getElementById("demo3").innerHTML = myFunction(10, 2); 
+// create an object with two properties and a method
+//   const person = {
+    // firstName:"happiness",
+    // lastName: "Daniel",
+    // fullName: function() {
+    //   return this.firstName + " " + this.lastName;
+    // }
+//   }
+//   document.getElementById("demo4").innerHTML = person.fullName(); 
+// fullName method of person is applied on person1
+  const person = {
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+
+  const person1 = {
+    firstName:"happiness",
+    lastName: "Daniel"
+  }
+  
+  document.getElementById("demo5").innerHTML = person.fullName.apply(person1); 
--- modules/grids/index.js
+++ modules/grids/index.js
@@ -1,1 +1,2 @@
 // create javascript code here
+

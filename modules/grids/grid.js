7339e765c8db6c4ca3ca33bfe1fc39ccfb868109
happiess77 <danielchinyere77@gmail.com>
2023-12-05 05:46:02 -0800

    practise on js function/class.

--- modules/grids/grid.js
+++ modules/grids/grid.js
@@ -1,1 +1,68 @@
 // create javascript code
+// Create Object from JSON String
+let text = '{"employees":[' +
+'{"firstName":"John","lastName":"Doe" },' +
+'{"firstName":"Anna","lastName":"Smith" },' +
+'{"firstName":"Peter","lastName":"Jones" }]}';
+
+const obj = JSON.parse(text);
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
+// JavaScript Debugger
+let x = 15 * 5;
+debugger;
+document.getElementById("demo2").innerHTML = x;
+// JavaScript Functions
+function myFunction(a, b) {
+    return a * b;
+  }
+  document.getElementById("demo3").innerHTML = myFunction(10, 2); 
+// create an object with two properties and a method
+  // const person = {
+    // firstName:"happiness",
+    // lastName: "Daniel",
+    // fullName: function() {
+      // return this.firstName + " " + this.lastName;
+    // }
+  // }
+  // document.getElementById("demo4").innerHTML = person.fullName(); 
+// fullName method of person is applied on person1
+  const person = {
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+
+  const person1 = {
+    firstName:"happiness",
+    lastName: "Daniel"
+  }
+  
+  document.getElementById("demo5").innerHTML = person.fullName.apply(person1); 
+   
+  // JavaScript Class Inheritance
+  // create a class named"model" which will inherit the methods from the "car"class:
+  // Use the "extends" keyword to inherit all methods from another class.
+  // Use the "super" method to call the parent's constructor function.
+
+  class Car {
+    constructor(brand) {
+      this.carname = brand;
+    }
+    present() {
+      return 'I have a ' + this.carname;
+    }
+  }
+  
+  class Model extends Car {
+    constructor(brand, mod) {
+      super(brand);
+      this.model = mod;
+    }
+    display() {
+      return this.present() + ', it is a ' + this.model;
+    }
+  }
+  
+  const myCar = new Model("toyota", "camary");
+  document.getElementById("demo6").innerHTML = myCar.display();
--- modules/grids/index.js
+++ modules/grids/index.js
@@ -1,63 +1,71 @@
 // create javascript code here
-// Create Object from JSON String
-let text = '{"employees":[' +
-'{"firstName":"John","lastName":"Doe" },' +
-'{"firstName":"Anna","lastName":"Smith" },' +
-'{"firstName":"Peter","lastName":"Jones" }]}';
-
-const obj = JSON.parse(text);
-document.getElementById("demo").innerHTML =
-obj.employees[1].firstName + " " + obj.employees[1].lastName;
-// JavaScript Debugger
-let x = 15 * 5;
-debugger;
-document.getElementById("demo2").innerHTML = x;
-// JavaScript Functions
-function myFunction(a, b) {
-    return a * b;
-  }
-  document.getElementById("demo3").innerHTML = myFunction(10, 2); 
-// create an object with two properties and a method
-  const person = {
-    firstName:"happiness",
-    lastName: "Daniel",
-    fullName: function() {
-      return this.firstName + " " + this.lastName;
-    }
-  }
-  document.getElementById("demo4").innerHTML = person.fullName(); 
-// fullName method of person is applied on person1
-  const person = {
-    fullName: function() {
-      return this.firstName + " " + this.lastName;
-    }
-  }
-
-  const person1 = {
-    firstName:"happiness",
-    lastName: "Daniel"
-  }
-  
-  document.getElementById("demo5").innerHTML = person.fullName.apply(person1); 
-   // JavaScript Function bind
+//  create an object with two properties and a method
+  // const person = {
+    // firstName:"happiness",
+    // lastName: "Daniel",
+    // fullName: function() {
+      // return this.firstName + " " + this.lastName;
+    // }
+  // }
+  // document.getElementById("demo").innerHTML = person.fullName();   
+// JavaScript Function bind
 
    // This example creates 2 objects (person and member)
    // The member object borrows the fullname method from person 
 
- const person = {
-     firstName:"victor",
-     lastName: "sunday",
-     fullName: function() {
-       return this.firstName + " " + this.lastName;
-      }
-   }
-  
-   const member = {
-    firstName:"Nelson",
-    lastName: "Mandella",
+  const person = {
+    firstName:"victor",
+    lastName: "sunday",
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+     }
   }
-  
-   let fullName = person.fullName.bind(member);
-  
-  document.getElementById("demo6").innerHTML = fullName();
-  
+ 
+  const member = {
+   firstName:"Nelson",
+   lastName: "Mandella",
+ }
+ 
+  let fullName = person.fullName.bind(member);
+ 
+ document.getElementById("demo2").innerHTML = fullName();
+
+//  JavaScript Function Closures
+//  Counting with a global variable.
+ // Initiate counter
+let counter = 0;
+
+// Function to increment counter
+function add() {
+  counter += 1;
+}
+
+// Call add() 6 times
+add();
+add();
+add();
+add();
+add();
+add();
+
+// The counter should now be 6
+document.getElementById("demo3").innerHTML = "The counter is: " + counter;
+
+// create a class method named"age",that returns the car age.
+
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+
+  }
+  age() {
+    const date = new Date();
+    return date.getFullYear() - this.year;
+  }
+}
+
+const myCar = new Car("Ford", 2013);
+document.getElementById("demo4").innerHTML =
+"My car is " + myCar.age() + " years old.";
+
